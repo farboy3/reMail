adcf1a25a79b5d0c0c6e789be9388719ea3c3de6
lenny <git@sweetleon.com>
2013-04-08 15:43:50 -0700

    use whichever build folder is latest

--- pieceable.sh
+++ pieceable.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-ditto -cz ${HOME}/Library/Developer/Xcode/DerivedData/ReMailIPhone-aqdgwymoyrycoegirxmnukuptime/Build/Products/Debug-iphonesimulator/reMail.app - | curl -F "email=lenny@hothouselabs.com" -F "file=@-" http://www.pieceable.com/view/publish
+ditto -cz `ls -td ${HOME}/Library/Developer/Xcode/DerivedData/ReMailIPhone-*`/Build/Products/Debug-iphonesimulator/reMail.app - |
+  curl -F "email=lenny@hothouselabs.com" -F "file=@-" http://www.pieceable.com/view/publish
